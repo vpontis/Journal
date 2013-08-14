9d9d0a77b4928e71c416397a96f5ed3446f1d51d
Victor Pontis <vpontis@palantir.com>
2013-08-14 01:31:07 -0700

    added splash page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Journal::Application.routes.draw do
+  root 'static_pages#home'
+  get "static_pages/home"
+  get "static_pages/about"
   resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
