16611cdfbb5a251a91ef3c4b6a940499d43dce7d
Victor Pontis <vpontis@Victors-MacBook-Air-2.local>
2013-08-20 20:56:14 -0400

    added user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Journal::Application.routes.draw do
+  get "users/new"
   root 'static_pages#home'
   get "static_pages/home"
   get "static_pages/about"
   resources :posts
+  match '/view_posts', to: 'posts#index', via: 'get'
+  match '/new_post', to: 'posts#new', via: 'get'
+  match '/signup', to: 'users#new', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
