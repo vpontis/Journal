29a58fef1e05773b2ef326d4879ca1688e830a21
Victor Pontis <vpontis@gmail.com>
2013-08-25 14:09:48 -0400

    finished sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: {maximum:50}
   validates :password, length: {minimum: 6}
   has_secure_password
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Journal::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signup', to: 'users#new', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 
   get "users/new"
   root 'static_pages#home'
@@ -8,7 +12,6 @@ Journal::Application.routes.draw do
   resources :posts
   match '/view_posts', to: 'posts#index', via: 'get'
   match '/new_post', to: 'posts#new', via: 'get'
-  match '/signup', to: 'users#new', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
